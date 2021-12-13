d4a97ddabefb4ef4b861eb2f5750e87b5cacfb7f
Husnul Aman <iam.amanxz@gmail.com>
2021-12-13 16:54:15 +0530

    basic theming completed

--- src/context/themeContext.jsx
+++ src/context/themeContext.jsx
@@ -1,30 +1,84 @@
 import { createContext, useContext, useState } from 'react';
 
 export const themes = {
-  auth: {
-    background: '#000',
-    backgroundHover: '#171717',
-    textPrimary: '#fff',
-    textSecondary: '#eee',
-    accent: '#69d100',
-    accentHover: '#4f9e00',
-    overlay: '#171717',
+  baseThemes: {
+    light: {
+      name: 'Light',
+      backgroundColor: '#fff',
+      backgroundLightColor: '#f7f9f9',
+      overlayColor: 'rgba(0,0,0,0.5)',
+      backgroundHoverColor: '#e2e8f0',
+      textPrimaryColor: '#000',
+      textSecondaryColor: '#000',
+      borderColor: '#e2e8f0',
+    },
+    dim: {
+      name: 'Dim',
+      backgroundColor: '#15202b',
+      backgroundLightColor: '#15202b',
+      overlayColor: '#0f172a',
+      backgroundHoverColor: '#0f172a',
+      textPrimaryColor: '#fff',
+      textSecondaryColor: '#fff',
+      borderColor: '#0f172a',
+    },
+    dark: {
+      name: 'Lights out',
+      backgroundColor: '#000',
+      backgroundLightColor: '#111',
+      overlayColor: '#0f172a',
+      backgroundHoverColor: '#0f172a',
+      textPrimaryColor: '#fff',
+      textSecondaryColor: '#fff',
+      borderColor: '#0f172a',
+    },
   },
-  light: {
-    foreground: '#000000',
-    background: '#eeeeee',
-  },
-  dark: {
-    foreground: '#ffffff',
-    background: '#222222',
+  accentThemes: {
+    colombo: {
+      textColor: '#fff',
+      accentColor: '#22c55e',
+      accentHoverColor: '#16a34a',
+    },
+    tokyo: {
+      textColor: '#fff',
+      accentColor: '#ef4444',
+      accentHoverColor: '#dc2626',
+    },
+    doha: {
+      textColor: '#fff',
+      accentColor: '#3b82f6',
+      accentHoverColor: '#2563eb',
+    },
+    osaka: {
+      textColor: '#fff',
+      accentColor: '#d946ef',
+      accentHoverColor: '#c026d3',
+    },
+    beijing: {
+      textColor: '#fff',
+      accentColor: '#f59e0b',
+      accentHoverColor: '#d97706',
+    },
+    denver: {
+      textColor: '#fff',
+      accentColor: '#9333ea',
+      accentHoverColor: '#7e22ce',
+    },
   },
 };
 
-const ThemeContext = createContext(themes.light);
+const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(themes.light);
-  const value = { theme, setTheme, themes };
+  const [baseTheme, setBaseTheme] = useState(themes.baseThemes.light);
+  const [accentTheme, setAccentTheme] = useState(themes.accentThemes.colombo);
+  const value = {
+    themes,
+    baseTheme,
+    setBaseTheme,
+    accentTheme,
+    setAccentTheme,
+  };
 
   return (
     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
